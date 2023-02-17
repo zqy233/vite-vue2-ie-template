cd642a03baf5640bd0be8f6dd6814d9d79f6ba80
zqylzcwcxy <347891134@qq.com>
2023-02-17 17:19:56 +0800

    :bug:fix bug : 去除echarts

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import "echarts"
-import Echarts from "vue-echarts"
-Vue.component("Echarts", Echarts)
+// import "echarts"
+// import Echarts from "vue-echarts"
+// Vue.component("Echarts", Echarts)
 
-import store from "./store"
-import { requireImg } from "@/utils/requireImg"
+import store from './store';
+import { requireImg } from '@/utils/requireImg';
 
-Vue.prototype.requireImg = requireImg
+Vue.prototype.requireImg = requireImg;
 
-import "@/assets/css/common.scss"
-import Loading from "element-ui/lib/loading"
-Vue.use(Loading.directive)
-import "element-ui/lib/theme-chalk/message.css"
-import "element-ui/lib/theme-chalk/notification.css"
-import VXETable from "vxe-table"
-import "vxe-table/lib/style.css"
-Vue.use(VXETable)
+import '@/assets/css/common.scss';
+import Loading from 'element-ui/lib/loading';
+Vue.use(Loading.directive);
+import 'element-ui/lib/theme-chalk/message.css';
+import 'element-ui/lib/theme-chalk/notification.css';
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
+Vue.use(VXETable);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app")
+  render: h => h(App),
+}).$mount('#app');
